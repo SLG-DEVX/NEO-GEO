11dfad3abf0a00f73fb116e8cc479e57eb48ced7
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-03-01 15:17:14 +0100

    Update index.js

--- Ovl_events/index.js
+++ Ovl_events/index.js
@@ -1,12 +1,9 @@
-import { dl_save_media_ms, recup_msg } from './fonctions.js'
-import message_upsert from './message_upsert.js'
-import group_participants_update from './group_participants_update.js'
-import connection_update from './connection.js'
+const { dl_save_media_ms, recup_msg } = require('./fonctions');
 
-export {
-  message_upsert,
-  group_participants_update,
-  connection_update,
+module.exports = {
+  message_upsert: require('./message_upsert'),
+  group_participants_update: require('./group_participants_update'),
+  connection_update: require('./connection'),
   dl_save_media_ms,
   recup_msg,
-}
+};
