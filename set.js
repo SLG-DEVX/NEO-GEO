c3d1327619680fc345b1fe995877fe19cb3a4676
SLG-DEVX <prudenceelise2009@gmail.com>
2026-03-01 15:42:51 +0100

    Update set.js

--- set.js
+++ set.js
@@ -1,5 +1,5 @@
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || 'ovl',
+    SESSION_ID: process.env.SESSION_ID || 'Ovl-MD_amnALrxr_SESSION-ID',
     PREFIXE: process.env.PREFIXE || "+",            
     MODE: process.env.MODE_PUBLIC || "public",
     NUMERO_OWNER: process.env.NUMERO_OWNER || "22605463559",
